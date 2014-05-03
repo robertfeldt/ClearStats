4188fa9a2513950bb8bf5d31842161f512b54078
Robert Feldt <robert.feldt@gmail.com>
2014-05-03 07:08:45 +0200

    fixed the same two data set plotting example, a bit slow though...

---
+++ R/density_plot.R
@@ -0,0 +1,36 @@
+library(ggplot2)
+
+parse_dataset_from_string <- function(string) {
+  # This works but does not parse 1e-3 etc:
+  # re <- "(\\d+\\.\\d+)|(\\d+)"
+  # m <- gregexpr(re, string)
+  #na.omit(as.numeric(unlist(regmatches(string, m))))
+
+  na.omit(as.numeric(unlist(strsplit(string, "\\s*(,|;|\\s+)\\s*"))))
+}
+
+data.frame.from.strings <- function(datasetAstr, datasetBstr) {
+  a <- parse_dataset_from_string(datasetAstr);
+  b <- parse_dataset_from_string(datasetBstr);
+
+  rbind(
+    data.frame(dataset = rep("A", times=length(a)), value=a), 
+    data.frame(dataset = rep("B", times=length(b)), value=b)
+  )  
+}
+
+density_plot <- function(datasetAstr, datasetBstr) {
+  data <- data.frame.from.strings(datasetAstr, datasetBstr);
+
+  p <- ggplot(data, aes(x = value, fill = dataset)) +
+        geom_density(alpha = 0.4) +
+        theme_bw();
+
+  medianA <- median(subset(data, dataset == "A")$value);
+  medianB <- median(subset(data, dataset == "B")$value);
+
+  p <- p + geom_vline(xintercept = medianA, linetype="dashed", size=0.3) +
+        geom_vline(xintercept = medianB, linetype="dotted", size=0.4);
+
+  print(p);
+}
