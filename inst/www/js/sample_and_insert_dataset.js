1b74ec0a9d42ba984418eea4d6b15b600d1b9bc6
Robert Feldt <robert.feldt@gmail.com>
2014-05-05 11:46:36 +0200

    better comment

--- inst/www/js/sample_and_insert_dataset.js
+++ inst/www/js/sample_and_insert_dataset.js
@@ -2,6 +2,7 @@
 //   Normal(20, 10) - 10 samples       => rnorm(10, mean = 20, sd = 10)
 //   Uniform(10, 30) - 20 samples      => runif(20, min = 10, max = 30)
 // and return an array with R function call name and arguments to produce that sample.
+// The R function call name must (currently) be an R function added in the R/sample.R file.
 function parseSamplingDescriptionToRCode(description) {
 	var match = /\s*([A-Za-z]+)\((\d+)\s*,\s*(\d+)\)\s+-\s+(\d+)\s+samples/i.exec(description);
 
