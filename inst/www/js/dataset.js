611023688a4e51742f98d33d7d7c2d816ce7f526
Robert Feldt <robert.feldt@gmail.com>
2014-05-05 09:00:14 +0200

    simplified code and moved the funcs related to parsing and comparing datasets to their own subdir

---
+++ inst/www/js/dataset.js
@@ -0,0 +1,44 @@
+function parse_dataset_from_string(str){
+  // Split then parse to float. Might return NaN for non-number strings.
+  var values = str.split(/\s*(?:,|;|\s+)\s*/).map(parseFloat);
+
+  // Filter so NaN is not returned.
+  var result = _.filter(values, function(n){ return !isNaN(n)});
+
+  return( result );
+}
+
+// Comparing objects for equality in Javascript is quite complex but this
+// should take care of it even if recursive hash-like objects etc.
+function isEqualRecursiveCompare(obj, reference){
+  if(obj === reference) return true;
+
+  if(obj.constructor !== reference.constructor) return false;
+
+  if(obj instanceof Array){
+
+    if(obj.length !== reference.length) return false;
+    for(var i=0, len=obj.length; i<len; i++){
+      if(typeof obj[i] == "object" && typeof reference[j] == "object"){
+        if(!isEqualRecursiveCompare(obj[i], reference[i])) return false;
+      } else if(obj[i] !== reference[i]) {return false;}
+    }
+
+  } else {
+
+    var objListCounter = 0;
+    var refListCounter = 0;
+    for(var i in obj){
+      objListCounter++;
+      if(typeof obj[i] == "object" && typeof reference[i] == "object"){
+        if(!isEqualRecursiveCompare(obj[i], reference[i])) return false;
+      }
+      else if(obj[i] !== reference[i]) return false;
+    }
+    for(var i in reference) refListCounter++;
+    if(objListCounter !== refListCounter) return false;
+
+  }
+
+  return true; //Every object and array is equal
+}
